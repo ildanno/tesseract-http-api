6303784249167018ac86ac16de3ad6354dacddcb
Daniele Contini <dannowonder@gmail.com>
2018-01-02 16:42:46 +0100

    basic implementation

--- api.go
+++ api.go
@@ -6,6 +6,9 @@ import (
 	"encoding/json"
 	"io"
 	"fmt"
+	"os/exec"
+	"bytes"
+	"strings"
 )
 
 type ApiRequestBody struct {
@@ -18,10 +21,12 @@ type ApiResponseBody struct {
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
-	r.HandleFunc("/api", ApiHandler).Methods("POST")
-	http.Handle("/", r)
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomeHandler)
+	router.HandleFunc("/api", ApiHandler).Methods("POST")
+	http.Handle("/", router)
+
+	fmt.Println("Up and running, listening on 0.0.0.0:16680")
 
 	http.ListenAndServe(":16680", nil)
 }
@@ -41,9 +46,25 @@ func ApiHandler(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
+	c1 := exec.Command("curl", "-s", apiRequestBody.ImageURL)
+	c2 := exec.Command("tesseract", "stdin", "stdout")
+
+	r, w := io.Pipe()
+	c1.Stdout = w
+	c2.Stdin = r
+
+	var b2 bytes.Buffer
+	c2.Stdout = &b2
+
+	c1.Start()
+	c2.Start()
+	c1.Wait()
+	w.Close()
+	c2.Wait()
+
 	encoder := json.NewEncoder(writer)
 	encoder.Encode(ApiResponseBody{
 		fmt.Sprintf("execution info for %s", apiRequestBody.ImageURL),
-		"parsed text",
+		strings.Trim(b2.String(), "\n"),
 	})
 }
